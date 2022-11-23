dae95b327e95b63f4ec782f3a6c4631d1d90137d
CherifYasmine <yasmine.chirif@insat.u-carthage.tn>
2022-11-23 19:03:48 +0100

    remove outer most parentheses problem

---
+++ LeetCodeProblems/removeOuterMostParentheses.py
@@ -0,0 +1,9 @@
+class Solution(object):
+    def removeOuterParentheses(self, s):
+        res, opened = [], 0
+        for c in s:
+            if c == '(' and opened > 0: res.append(c)
+            if c == ')' and opened > 1: res.append(c)
+            opened += 1 if c == '(' else -1
+        return "".join(res)
+        
